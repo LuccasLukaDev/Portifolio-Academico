0cb9583b67ebd9af8f064b220947e8cb4b9255c4
LuccasLukaDev <luccaslukadev@gmail.com>
2026-04-17 21:48:24 -0300

    fix: Adequação para o git pages

--- script.js
+++ script.js
@@ -302,21 +302,8 @@ function carregarCursos() {
 
 }
 
-function abrirImagem(base64) {
-  const byteString = atob(base64.split(",")[1]);
-  const mimeString = base64.split(",")[0].split(":")[1].split(";")[0];
-
-  const ab = new ArrayBuffer(byteString.length);
-  const ia = new Uint8Array(ab);
-
-  for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-
-  const blob = new Blob([ab], { type: mimeString });
-  const url = URL.createObjectURL(blob);
-
-  window.open(url, "_blank");
+function abrirImagem(caminho) {
+  window.open(caminho, "_blank");
 }
 
 function carregarProjetos() {
